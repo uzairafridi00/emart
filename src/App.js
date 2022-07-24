d6c8aa1723f837010b86350ee9fc5b78083a13c8
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-24 12:59:06 +0500

    Error Page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Products from "./components/product/Products";
+import PageNotFound from "./components/errors/PageNotFound";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route path="/products" element={<Products />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
