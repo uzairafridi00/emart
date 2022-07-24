834e916f522f7dea8c9819972120eb45fc2876e1
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-24 12:47:53 +0500

    filter functionality added to Show Products

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -22,46 +22,18 @@ function Products() {
   }, []);
 
   return (
-
     <div className="container my-5 py-5">
       <div className="row">
         <div className="col-12 mb-5">
-          <h1 className="display-6 fw-bolder text-center">
-            Latest Products
-          </h1>
+          <h1 className="display-6 fw-bolder text-center">Latest Products</h1>
           <hr />
         </div>
       </div>
 
       <div className="row justify-content-center">
-        {loading ? <Loading /> : <ShowProducts data={data}/>}
+        {loading ? <Loading /> : <ShowProducts data={data} />}
       </div>
-
-</div> 
-
-    // Seperate Components have been made for ShowProducts and Loading 
-
-    // <div className="products-container">
-    //   {loading && (
-    //     <div>
-    //       <h1>Loading...</h1>
-    //     </div>
-    //   )}
-
-    //   {data.map((product) => (
-    //     <div key={product.id} className="card" style={{width: "18rem"}}>
-    //       <img src={product.image} className="card-img-top" alt="..." />
-    //       <div className="card-body">
-    //         <h5 className="card-title">{product.title}</h5>
-    //         <p className="card-text">{product.category}</p>
-    //         <a
-    //           href="#"
-    //           className="btn btn-primary"
-    //         >{`Price: ${product.price}`}</a>
-    //       </div>
-    //     </div>
-    //   ))}
-    // </div>
+    </div>
   );
 }
 
--- src/components/product/ShowProducts.jsx
+++ src/components/product/ShowProducts.jsx
@@ -1,28 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ShowProducts(props) {
+  const [filter, setFilter] = useState(props.data);
+
+  const filterProduct = (cat) => {
+    const updatedList = props.data.filter((x) => x.category === cat);
+    console.log(updatedList);
+    setFilter(updatedList);
+  };
+
   return (
     <>
       <div className="buttons d-flex justify-content-center mb-5 pb-5">
-        <button className="btn btn-outline-dark">All</button>
-        <button className="btn btn-outline-dark ms-2">Men Collection</button>
-        <button className="btn btn-outline-dark ms-2">Women Collection</button>
-        <button className="btn btn-outline-dark ms-2">
+        <button
+          className="btn btn-outline-dark"
+          onClick={() => setFilter(props.data)}
+        >
+          All
+        </button>
+        <button
+          className="btn btn-outline-dark ms-2"
+          onClick={() => filterProduct("men's clothing")}
+        >
+          Men Collection
+        </button>
+        <button
+          className="btn btn-outline-dark ms-2"
+          onClick={() => filterProduct("women's clothing")}
+        >
+          Women Collection
+        </button>
+        <button
+          className="btn btn-outline-dark ms-2"
+          onClick={() => filterProduct("jewelery")}
+        >
           Jewellary Collection
         </button>
-        <button className="btn btn-outline-dark ms-2">Electronics</button>
+        <button
+          className="btn btn-outline-dark ms-2"
+          onClick={() => filterProduct("electronics")}
+        >
+          Electronics
+        </button>
       </div>
 
-      {props.data.map((product) => (
-        <div key={product.id} className="card ms-4" style={{width: "18rem"}}>
-          <img src={product.image} className="card-img-top" alt="..." />
+      {filter.map((product) => (
+        <div
+          key={product.id}
+          className="card ms-4"
+          style={{ width: "18rem", cursor: "pointer", margin: "10px 0" }}
+        >
+          <img
+            src={product.image}
+            className="card-img-top"
+            alt={product.title}
+          />
           <div className="card-body">
-            <h5 className="card-title">{product.title}</h5>
-            <p className="card-text">{product.category}</p>
-            <a
-              href="#"
-              className="btn btn-primary"
-            >{`Price: ${product.price}`}</a>
+            <h5 className="card-title">{product.title.substring(0, 12)}</h5>
+            <p className="card-text lead fw-bold">{`Price: ${product.price}`}</p>
+            <a href="#" className="btn btn-outline-dark">
+              Buy Now
+            </a>
           </div>
         </div>
       ))}
