a7d475a28d19965dcfdf79b7da15a7421e5dd85a
Uzair Afridi <uzairafridi00@gmail.com>
2022-07-24 12:48:20 +0500

    react-loading-skeleton added

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import Skeleton from "react-loading-skeleton";
 
 function Loading() {
   return (
     <>
-      <section>
-        <p>Loading...</p>
-      </section>
+      <div className="col-md-3">
+        <Skeleton height={350} />
+      </div>
+      <div className="col-md-3">
+        <Skeleton height={350} />
+      </div>
+      <div className="col-md-3">
+        <Skeleton height={350} />
+      </div>
+      <div className="col-md-3">
+        <Skeleton height={350} />
+      </div>
     </>
   );
 }
